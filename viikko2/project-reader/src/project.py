e83ad11cd94a4ad97413c3469855b1603cfd30ba
mikaelri <mikael.rintala@outlook.com>
2023-11-02 12:00:29 +0200

    t.2 palautus

---
+++ viikko2/project-reader/src/project.py
@@ -0,0 +1,29 @@
+class Project:
+    def __init__(self, name, description, license, authors, dependencies, dev_dependencies):
+        self.name = name
+        self.description = description
+        self.license = license
+        self.authors = authors
+        self.dependencies = dependencies
+        self.dev_dependencies = dev_dependencies
+
+
+    def _stringify_dependencies(self, dependencies):
+        return "\n - ".join(dependencies) if len(dependencies) > 0 else "-"
+    
+    def _stringify_authors(self, authors):
+        return "\n - ".join(authors) if authors else "-"
+    
+
+    def __str__(self):
+        return (
+            f"Name: {self.name}"
+            f"\nDescription: {self.description or '-'}"
+            f"\nLicense: {self.license or '-'}"
+            f"\n"
+            f"\nAuthors:\n - {self._stringify_authors(self.authors)}"
+            f"\n"
+            f"\nDependencies:\n - {self._stringify_dependencies(self.dependencies)}"
+            f"\n"
+            f"\nDevelopment dependencies:\n - {self._stringify_dependencies(self.dev_dependencies)}"
+        )
---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,25 @@
+from urllib import request
+from project import Project
+import toml
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        #testiprinttaus: print(content)
+
+        parsed_toml = toml.loads(content)
+        #testiprinttaus: print(parsed_toml)
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(
+            parsed_toml["tool"]["poetry"]["name"],
+            parsed_toml["tool"]["poetry"]["description"],
+            parsed_toml["tool"]["poetry"]["license"],    
+            parsed_toml["tool"]["poetry"]["authors"],    
+            parsed_toml["tool"]["poetry"]["dependencies"],
+            parsed_toml["tool"]["poetry"]["group"]["dev"]["dependencies"])
