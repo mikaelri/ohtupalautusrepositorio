73dee1f72a3a25cb9a6fe94f7bb9ab26916126fe
mikaelri <mikael.rintala@outlook.com>
2023-11-06 17:05:52 +0200

    debug printtausten poisto

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -4,17 +4,14 @@ from player import Player
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
     response = requests.get(url).json()
-
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
+    
     players = []
 
     for player_dict in response:
         player = Player(player_dict)
         players.append(player)
 
-    print("Oliot:")
+
     print("Players from FIN \n")
 
     for player in players:
