b2a66aeab4c82b23081c793ccfc86ad79dcbdf7d
mikaelri <mikael.rintala@outlook.com>
2023-11-21 13:10:00 +0200

    t.2 palautus

--- viikko4/maksukortti-mock/src/kassapaate.py
+++ viikko4/maksukortti-mock/src/kassapaate.py
@@ -6,8 +6,14 @@ class Kassapaate:
         self.myytyja_lounaita = 0
 
     def lataa(self, kortti, summa):
-        kortti.lataa(summa)
+        if summa > 0:
+            kortti.lataa(summa)
+        else:
+            pass
 
     def osta_lounas(self, kortti):
-        kortti.osta(HINTA)
-        self.myytyja_lounaita = self.myytyja_lounaita + 1
+        if kortti.saldo >= HINTA:
+            kortti.osta(HINTA)
+            self.myytyja_lounaita = self.myytyja_lounaita + 1
+        else:
+            pass
--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,19 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_kortille_voi_ladata_positiivisen_summan(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+
+        self.kassa.lataa(maksukortti_mock, 2)
+
+        maksukortti_mock.lataa.assert_called_with(2)
+
+    def test_kortille_ei_voi_ladata_negatiivista_summaa(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 4
+
+        self.kassa.lataa(maksukortti_mock, -4)
+
+        maksukortti_mock.lataa.assert_not_called()
