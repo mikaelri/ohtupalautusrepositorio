985f85ceac7b98aa6c3d09d09339d3de012b587d
mikaelri <mikael.rintala@outlook.com>
2023-11-29 12:42:22 +0200

    return ex. 5.4, 5.5

--- viikko5/laskin/src/kayttoliittyma.py
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from tkinter import ttk, constants, StringVar
+from komentotehdas import Summa, Erotus, Nollaus, Kumoa
 
 
 class Komento(Enum):
@@ -10,13 +11,20 @@ class Komento(Enum):
 
 
 class Kayttoliittyma:
-    def __init__(self, sovellus, root):
-        self._sovellus = sovellus
+    def __init__(self, sovelluslogiikka, root):
+        self._sovelluslogiikka = sovelluslogiikka
         self._root = root
+
+        self._komennot = {
+            Komento.SUMMA: Summa(self._sovelluslogiikka, self._lue_syote),
+            Komento.EROTUS: Erotus(self._sovelluslogiikka, self._lue_syote),
+            Komento.NOLLAUS: Nollaus(self._sovelluslogiikka, self._lue_syote),
+            Komento.KUMOA: Kumoa(sovelluslogiikka, self._lue_syote)
+        }
 
     def kaynnista(self):
         self._arvo_var = StringVar()
-        self._arvo_var.set(self._sovellus.arvo())
+        self._arvo_var.set(self._sovelluslogiikka.arvo())
         self._syote_kentta = ttk.Entry(master=self._root)
 
         tulos_teksti = ttk.Label(textvariable=self._arvo_var)
@@ -54,29 +62,18 @@ class Kayttoliittyma:
         self._nollaus_painike.grid(row=2, column=2)
         self._kumoa_painike.grid(row=2, column=3)
 
+    def _lue_syote(self):
+        return self._syote_kentta.get()
+
     def _suorita_komento(self, komento):
-        arvo = 0
-
-        try:
-            arvo = int(self._syote_kentta.get())
-        except Exception:
-            pass
-
-        if komento == Komento.SUMMA:
-            self._sovellus.plus(arvo)
-        elif komento == Komento.EROTUS:
-            self._sovellus.miinus(arvo)
-        elif komento == Komento.NOLLAUS:
-            self._sovellus.nollaa()
-        elif komento == Komento.KUMOA:
-            pass
-
+        komento_olio = self._komennot[komento]
+        komento_olio.suorita()
         self._kumoa_painike["state"] = constants.NORMAL
 
-        if self._sovellus.arvo() == 0:
+        if self._sovelluslogiikka.arvo() == 0:
             self._nollaus_painike["state"] = constants.DISABLED
         else:
             self._nollaus_painike["state"] = constants.NORMAL
 
         self._syote_kentta.delete(0, constants.END)
-        self._arvo_var.set(self._sovellus.arvo())
+        self._arvo_var.set(self._sovelluslogiikka.arvo())
--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -1,18 +1,28 @@
 class Sovelluslogiikka:
     def __init__(self, arvo=0):
         self._arvo = arvo
+        self._edeltava_arvo = 0
 
     def miinus(self, operandi):
+        self._edeltava_arvo = self._arvo
         self._arvo = self._arvo - operandi
 
     def plus(self, operandi):
+        self._edeltava_arvo = self._arvo
         self._arvo = self._arvo + operandi
 
     def nollaa(self):
+        self._edeltava_arvo = self._arvo
         self._arvo = 0
 
     def aseta_arvo(self, arvo):
+        self._edeltava_arvo = self._arvo
         self._arvo = arvo
+
+    def kumoa(self):
+        nykyinen_arvo = self._arvo
+        self._arvo = self._edeltava_arvo
+        self._edeltava_arvo = nykyinen_arvo
 
     def arvo(self):
         return self._arvo
